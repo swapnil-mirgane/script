51e3b74ec605fc735341bbccf8055014370fe7a3
Swapnil Mirgane <swapnil.mirgane@corp.ola.com>
2022-07-11 11:51:52 +0530

    add first

---
+++ arr.js
@@ -0,0 +1,12 @@
+let arr=[1,2,3,4,5];  // slice,splice,loops, 2 push 2 pop 
+// let len=arr.length/2;
+// console.log(Math.floor(len));
+// var c=arr.splice(len,1);
+// console.log(arr);
+// [a,b,,c,d]=arr;
+// arr=[a,b,c,d];
+// console.log(arr);
+arr.filter((val,i)=>{
+if(val==3){
+}console.log(val,i);
+});
